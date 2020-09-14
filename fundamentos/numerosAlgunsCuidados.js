18ccce7e047b226be49b08b0f1353a37385da85e
Felipe Martins <felipemjesus@outlook.com>
2020-09-14 11:26:52 -0300

    Number: Alguns Cuidados

---
+++ fundamentos/numerosAlgunsCuidados.js
@@ -0,0 +1,6 @@
+console.log(1 / 0); // retorna infinity
+console.log("10" / 2); // consegui realizar a divisão
+console.log("Show!" * 2); // retorna NaN - not number
+console.log(0.1 + 0.7); // não é sempre que retorna o valor exato, no caso de números flutuante
+// console.log(10.toString()); // gera um erro
+console.log((10.345).toFixed(2)); // sem a necessidade de criar váriavel
