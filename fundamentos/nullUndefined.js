2e761eb383ba287522ca5186b74ad8f2c7364f14
Felipe Martins <felipemjesus@outlook.com>
2020-09-14 19:13:25 -0300

    Entendendo o Null & Undefined

---
+++ fundamentos/nullUndefined.js
@@ -0,0 +1,20 @@
+let valor; // não inicializada
+console.log(valor);
+
+valor = null; // ausência de valor
+console.log(valor);
+//console.log(valor.toString()); // Erro
+
+const produto = {};
+console.log(produto.preco);
+console.log(produto);
+
+produto.preco = 1.50;
+console.log(produto);
+
+produto.preco = undefined; // evite atribuir undefined
+console.log(!!produto.preco);
+// delete produto.preco; // remove a chave preco do objeto
+console.log(produto);
+
+produto.preco = null; //sem preço
