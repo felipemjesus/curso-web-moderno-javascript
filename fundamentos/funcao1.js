098b42d876295c57f345e44987f39843e9b45290
Felipe Martins <felipemjesus@outlook.com>
2020-09-14 19:52:44 -0300

    Exemplos Básicos de Funções #01

---
+++ fundamentos/funcao1.js
@@ -0,0 +1,16 @@
+// Função sem retorno
+function imprimirSoma(a, b) {
+    console.log(a + b);
+}
+imprimirSoma(1, 2);
+imprimirSoma(1);
+imprimirSoma(1, 2, 3, 4);
+imprimirSoma();
+
+// Função com retorno
+function soma(a, b = 1) {
+    return a + b;
+}
+console.log(soma(1, 2));
+console.log(soma(1));
+console.log(soma());
