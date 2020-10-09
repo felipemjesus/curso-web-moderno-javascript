b83283211ad344b2363e46be5a857ae7edeaff0b
Felipe Martins <felipemjesus@outlook.com>
2020-10-09 17:07:54 -0300

    Objeto - Notação Literal

---
+++ objeto/notacaoLiteral.js
@@ -0,0 +1,27 @@
+const a = 1
+const b = 1
+const c = 1
+
+const obj1 = { a: a, b: b, c: c }
+const obj2 = { a, b, c }
+console.log(obj1, obj2)
+
+const nomeAttr = 'nota'
+const valorAttr = 7.56
+
+const obj3 = {}
+obj3[nomeAttr] = valorAttr
+console.log(obj3)
+
+const obj4 = {[nomeAttr]: valorAttr}
+console.log(obj4)
+
+const obj5 = {
+  funcao1: function() {
+
+  },
+  funcao2() {
+
+  }
+}
+console.log(obj5)
