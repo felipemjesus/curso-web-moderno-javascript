20ae1c66af064005eeab49ff2c416cc58371d312
Felipe Martins <felipemjesus@outlook.com>
2020-10-12 11:20:45 -0300

    Node - Resolvendo Desafio do axios

--- node/funcionarios/funcionaios.js
+++ node/funcionarios/funcionaios.js
@@ -4,5 +4,26 @@ const url = 'http://files.cod3r.com.br/curso-js/funcionarios.json'
 
 axios.get(url).then(response => {
   const funcionarios = response.data
-  console.log(funcionarios)
+  //console.log(funcionarios)
+})
+
+// Desafio - recuperar mulher chinesa que possui o menor salário
+
+axios.get(url).then(response => {
+  
+  const funcionarios = response.data
+  
+  const getChineses = f => f.pais == 'China'
+  const getMulheres = f => f.genero == 'F'
+  const getMenorSalario = (funcionario, funcionarioAtual) => {
+    return funcionario.salario < funcionarioAtual.salario 
+      ? funcionario 
+      : funcionarioAtual
+  }
+  const mulher = funcionarios
+    .filter(getChineses)
+    .filter(getMulheres)
+    .reduce(getMenorSalario)
+  console.log(mulher)
+
 })
