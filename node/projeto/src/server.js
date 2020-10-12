edf74f18a7a1e726d311b4e604e20bb69e9e14e6
Felipe Martins <felipemjesus@outlook.com>
2020-10-12 16:49:40 -0300

    Node - Projeto: API com Express #02

--- node/projeto/src/server.js
+++ node/projeto/src/server.js
@@ -1,11 +1,17 @@
 const express = require('express')
 const app = express()
+const db = require('./database')
 
 app.get('/produtos', (request, response, next) => {
-  response.send({
-    nome: 'Notebook',
-    preco: 123.45
-  })
+  response.send(db.getProdutos())
+})
+
+app.get('/produtos/:id', (request, response, next) => {
+  response.send(db.getProduto(request.params.id))
+})
+
+app.post('/produtos', (request, response, next) => {
+  response.send(db.saveProduto(request.body))
 })
 
 const port = 3003
